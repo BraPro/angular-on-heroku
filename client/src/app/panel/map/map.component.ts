e7529b290c8bbe4bed87e6dd22bf0e624162455b
oreliyahu <oreliyahu93@gmail.com>
2020-05-13 21:55:57 +0300

    overall fixed - map

--- client/src/app/panel/map/map.component.ts
+++ client/src/app/panel/map/map.component.ts
@@ -1,8 +1,8 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { MapDialogBoxComponent } from '../dialog-box/map-dialog-box.component';
 import { MatDialog } from '@angular/material/dialog';
-import { Garage, Location } from '../../_models'
-import { GarageService, UserService } from '../../_services';
+import { Garage } from '@app/_models'
+import { GarageService, UserService } from '@app/_services';
 import { SharedService } from '@app/shared/shared.service';
 import { first } from 'rxjs/operators';
 
@@ -50,7 +50,7 @@ export class MapComponent implements AfterViewInit {
   }
   
   //Coordinates to set the center of the map
- // coordinates = new google.maps.LatLng(this.lat, this.lng);
+  // coordinates = new google.maps.LatLng(this.lat, this.lng);
 
   mapOptions: google.maps.MapOptions = {
     center: this.israel,
@@ -70,12 +70,10 @@ export class MapComponent implements AfterViewInit {
    this.selectControl(select);
    this.map.controls[google.maps.ControlPosition.LEFT_TOP].push(select);
 
-    //Adding markers
     this.loadAllMarkers(this.map , this.markers , this.userService.getUserPermission() );
   }
 
   loadAllMarkers(themap: google.maps.Map, markers , permission): void {
-
     this.garageService.getAll().pipe(first())
 		.subscribe(data => {
         this.garages=data;
@@ -112,101 +110,86 @@ export class MapComponent implements AfterViewInit {
 			});
     } 
 
-    selectControl(controlDiv){
-      // Set CSS for the control interior.
-      var control=this;
-      if(this.userService.getUserPermission() == 'Admin'){
-      var controlAdd = document.createElement('img');
-      controlAdd.style.paddingLeft = '20px';
-      controlAdd.style.paddingRight = '35px';
-      controlAdd.srcset="./assets/img/plus-emoji.png";
-      controlAdd.title ="Add New Garage";
-      controlDiv.appendChild(controlAdd);
+  selectControl(controlDiv){
+    // Set CSS for the control interior.
+    var control=this;
+    if(this.userService.getUserPermission() == 'Admin'){
+    var controlAdd = document.createElement('img');
+    controlAdd.style.paddingLeft = '20px';
+    controlAdd.style.paddingRight = '35px';
+    controlAdd.srcset="./assets/img/plus-emoji.png";
+    controlAdd.title ="Add New Garage";
+    controlDiv.appendChild(controlAdd);
 
-      controlAdd.addEventListener('mouseenter', function() {
-        controlAdd.style.cursor = "pointer";});
+    controlAdd.addEventListener('mouseenter', function() {
+      controlAdd.style.cursor = "pointer";});
 
-      controlAdd.addEventListener('mouseleave', function() {
-        controlAdd.style.cursor = "default"});
+    controlAdd.addEventListener('mouseleave', function() {
+      controlAdd.style.cursor = "default"});
 
-      controlAdd.addEventListener('click', function() {
-        control.openDialog('Add' ,null);});
-      }
-     
+    controlAdd.addEventListener('click', function() {
+      control.openDialog('Add' ,null);});
     }
+  }
 
-    findLatLang(address, geocoder) {
-      return new Promise(function(resolve, reject) {
-          geocoder.geocode({'address': address}, function(results, status) {
-              if (status === 'OK') {
-                  resolve({lat : results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()});
-              } else {
-                  reject(new Error('Couldnt\'t find the location ' + address));
-              }
+  findLatLang(address, geocoder) {
+    return new Promise(function(resolve, reject) {
+        geocoder.geocode({'address': address}, function(results, status) {
+            if (status === 'OK') {
+                resolve({lat : results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()});
+            } else {
+                reject(new Error('Couldnt\'t find the location ' + address));
+            }
       })
-      })
+    });
   } 
    
 
-    async AddGarage(garage:any, garageService: GarageService, sharedService: SharedService)
-    {
-       var geocoder = new google.maps.Geocoder();
-       let locationData = [];
-       locationData.push(this.findLatLang(garage.location.street+', '+garage.location.city+', '+garage.location.country, geocoder))
-       Promise.all(locationData)     
-      .then(function(returnVals){
+  async AddGarage(garage:any, garageService: GarageService, sharedService: SharedService) {
+      var geocoder = new google.maps.Geocoder();
+      let locationData = [];
+      locationData.push(this.findLatLang(garage.location.street+', '+garage.location.city+', '+garage.location.country, geocoder))
+      Promise.all(locationData).then(function(returnVals){
         garage.location.position = returnVals[0]; 
-        garageService.add(garage)
-         .pipe(first())
-          .subscribe(
-         data => {
+        garageService.add(garage).pipe(first())
+        .subscribe(data => {
             sharedService.sendAlertEvent(data);
             this.RemoveAllmarkers();
             this.loadAllMarkers(this.map , this.markers , this.userService.getUserPermission());
             this.map.panTo(returnVals[0]);
-         });
+        });
       }.bind(this));
-    }
+  }
 
-   
-
-    EditGarage(garage:Garage){
-      var geocoder = new google.maps.Geocoder();
-      let locationData = [];
-      locationData.push(this.findLatLang(garage.location.street+', '+garage.location.city+', '+garage.location.country, geocoder))
-      Promise.all(locationData)     
-     .then(function(returnVals){
+  EditGarage(garage:Garage){
+    var geocoder = new google.maps.Geocoder();
+    let locationData = [];
+    locationData.push(this.findLatLang(garage.location.street+', '+garage.location.city+', '+garage.location.country, geocoder))
+    Promise.all(locationData).then(function(returnVals){
       garage.location.position = returnVals[0]; 
-      this.garageService.update(garage)
-      .pipe(first())
-       .subscribe(
-      data => {
+      this.garageService.update(garage).pipe(first())
+      .subscribe(data => {
         this.sharedService.sendAlertEvent(data);  
         this.RemoveAllmarkers();
         this.loadAllMarkers(this.map , this.markers , this.userService.getUserPermission());   
-      });}.bind(this))
-
-    }
-   
-    DeleteGarage(garage:Garage){
-      this.garageService.delete(garage._id)
-      .pipe(first())
-       .subscribe(
-      data => {
-          this.RemoveAllmarkers();
-          this.loadAllMarkers(this.map , this.markers , this.userService.getUserPermission());
-          this.sharedService.sendAlertEvent(data);
       });
-
-    }
-
-    RemoveAllmarkers(){
-      this.markers.forEach(marker => {
-         marker.setMap(null);
-      });
-
-      this.markers=[];
-      this.garages=[];
-    }
-
+    }.bind(this))
   }
+   
+  DeleteGarage(garage:Garage){
+    this.garageService.delete(garage._id).pipe(first())
+    .subscribe(data => {
+        this.RemoveAllmarkers();
+        this.loadAllMarkers(this.map , this.markers , this.userService.getUserPermission());
+        this.sharedService.sendAlertEvent(data);
+    });
+  }
+
+  RemoveAllmarkers(){
+    this.markers.forEach(marker => {
+        marker.setMap(null);
+    });
+    this.markers=[];
+    this.garages=[];
+  }
+}
