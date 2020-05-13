20c7506b9e6706c456692c3589bb17f3ce8525fb
BraPro <lopesmoshe@hotmail.com>
2020-05-13 11:19:33 +0300

    account fixing**

--- client/src/app/account/account.component.ts
+++ client/src/app/account/account.component.ts
@@ -175,6 +175,7 @@ export class AccountComponent implements OnInit {
 			data => {
 				//this.alertService.success('Registration successful', true);
 				this.sharedService.sendAlertEvent(data);
+				this.ngOnInit();
 				//if(data.response == 'Success'){
 					//setTimeout(() => {  this.router.navigate(['/login']); }, 1000);
 				//}
